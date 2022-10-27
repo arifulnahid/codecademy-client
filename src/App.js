7ca023dafec6a542e2f45c6ab5a9cc364d30c6b0
arifulnahid <ainahidfsti@gmail.com>
2022-10-27 20:52:22 +0600

    add setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/routes';
 
 function App() {
   return (
-    <div data-theme="light" className="App">
-
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
